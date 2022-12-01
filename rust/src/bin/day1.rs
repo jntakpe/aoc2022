ce03adbd3f14a52fc11801fff5e3daa5f74d2d2e
jocelyn.ntakpe <jocelyn.ntakpe@manomano.com>
2022-12-01 15:57:11 +0100

    init rust project and first day solution

---
+++ rust/src/bin/day1.rs
@@ -0,0 +1,71 @@
+use aoc_2022::shared::{Day, read_input_lines_preserving_empty};
+
+fn main() {
+    Day1 {
+        input: read_input_lines_preserving_empty(1)
+    }.run()
+}
+
+struct Day1 {
+    input: Vec<String>,
+}
+
+impl Day1 {
+    fn group_elf(&self) -> Vec<usize> {
+        self.input
+            .split(|s| s.is_empty())
+            .map(|l| l.iter().map(|s| s.parse::<usize>().unwrap()).sum())
+            .collect()
+    }
+}
+
+impl Day for Day1 {
+    fn part1(&self) -> usize {
+        *self.group_elf()
+            .iter()
+            .max()
+            .unwrap()
+    }
+
+    fn part2(&self) -> usize {
+        let mut elves = self.group_elf();
+        elves.sort_by(|a, b| b.cmp(a));
+        elves.iter().take(3).sum()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_sample() {
+        assert_eq!(sample_day().part1(), 24000)
+    }
+
+    #[test]
+    fn part2_sample() {
+        assert_eq!(sample_day().part2(), 45000)
+    }
+
+    fn sample_day() -> Day1 {
+        Day1 {
+            input: vec![
+                "1000",
+                "2000",
+                "3000",
+                "",
+                "4000",
+                "",
+                "5000",
+                "6000",
+                "",
+                "7000",
+                "8000",
+                "9000",
+                "",
+                "10000",
+            ].iter().map(|s| s.to_string()).collect()
+        }
+    }
+}
---
+++ rust/src/shared.rs
@@ -0,0 +1,46 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::time::SystemTime;
+
+pub fn read_input_numbers(day: usize) -> Vec<usize> {
+    read_input_lines(day)
+        .iter()
+        .filter_map(|i| i.parse().ok())
+        .collect()
+}
+
+pub fn read_input_lines(day: usize) -> Vec<String> {
+    read_lines(day, false)
+}
+
+pub fn read_input_lines_preserving_empty(day: usize) -> Vec<String> {
+    read_lines(day, true)
+}
+
+fn read_lines(day: usize, preserve_empty: bool) -> Vec<String> {
+    BufReader::new(open_file(day))
+        .lines()
+        .filter_map(Result::ok)
+        .filter(|s| preserve_empty || !s.trim().is_empty())
+        .collect()
+}
+
+fn open_file(day: usize) -> File {
+    let filename = format!("inputs/day_{}.txt", day);
+    File::open(&filename).unwrap_or_else(|_| panic!("Unable to open file: '{}'", &filename))
+}
+
+pub trait Day {
+    fn part1(&self) -> usize;
+
+    fn part2(&self) -> usize;
+
+    fn run(&self) {
+        let now = SystemTime::now();
+        let part1 = self.part1();
+        println!("Part 1: {} in {} ms", part1, now.elapsed().unwrap().as_millis());
+        let now = SystemTime::now();
+        let part2 = self.part2();
+        println!("Part 2: {} in {} ms", part2, now.elapsed().unwrap().as_millis());
+    }
+}
