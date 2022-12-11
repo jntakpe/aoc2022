41dcbc981dd5488e460cb33ea6b6b3aa57c6a673
jocelyn.ntakpe <jocelyn.ntakpe@manomano.com>
2022-12-11 18:03:21 +0100

    better day 11 solution

--- kotlin/src/main/kotlin/com/github/jntakpe/aoc2022/days/Day11.kt
+++ kotlin/src/main/kotlin/com/github/jntakpe/aoc2022/days/Day11.kt
@@ -2,125 +2,101 @@ package com.github.jntakpe.aoc2022.days
 
 import com.github.jntakpe.aoc2022.shared.Day
 import com.github.jntakpe.aoc2022.shared.readInputSplitOnBlank
-import java.util.function.Predicate
 
 object Day11 : Day {
 
-    override val input = readInputSplitOnBlank(11).map { Monkey.parse(it) }
+    override val input: List<Input> = readInputSplitOnBlank(11).map { parse(it) }
 
-    override fun part1(): Long {
-        val input = readInputSplitOnBlank(11).map { Monkey.parse(it) }
-        repeat(20) {
-            input.forEach {
-                val round = it.round()
-                round.forEach { (k, v) -> input[k].items += v }
-            }
-        }
-        val (first, second) = input.map { it.inspected }.sortedDescending().take(2)
-        return first * second
+    override fun part1() = solve(20) { it / 3 }
+
+    override fun part2() = solve(10000) { it % input.map { i -> i.denominator.toLong() }.reduce(Long::times) }
+
+    private fun solve(rounds: Int, worryReducer: (Long) -> Long): Long {
+        val monkeys = input.map { Monkey(it.worryLevels, it.rules) }
+        repeat(rounds) { monkeys.forEach { m -> m.round(worryReducer).forEach { (k, v) -> monkeys[k].add(v) } } }
+        val (a, b) = monkeys.map { it.inspected }.sortedDescending()
+        return a * b
     }
 
-    override fun part2(): Long {
-        val input = readInputSplitOnBlank(11).map { Monkey.parse(it) }
-        val mod = input.map { it.divided }.reduce(Long::times)
-        repeat(10000) {
-            input.forEach {
-                val round = it.round2(mod)
-                round.forEach { (k, v) -> input[k].items += v }
-            }
-        }
-        val map = input.map { it.inspected }
-        val (first, second) = map.sortedDescending().take(2)
-        return first * second
-    }
-
-    class Monkey(
-        var items: MutableList<Long>,
-        private val operation: (worryLevel: Long) -> Long,
-        private val test: Predicate<Long>,
-        private val throwIfTrue: Int,
-        private val throwIfFalse: Int,
-        val divided: Long
-    ) {
+    class Monkey(private var worryLevels: List<Long>, private val rules: Rules) {
 
         var inspected = 0L
+            private set
 
-        fun round(): Map<Int, MutableList<Long>> {
-            return buildMap {
-                items.forEach {
-                    inspected++
-                    val worryLevel = operation.invoke(it) / 3
-                    if (test.test(worryLevel)) {
-                        compute(throwIfTrue) { _, v -> v?.apply { add(worryLevel) } ?: mutableListOf(worryLevel) }
-                    } else {
-                        compute(throwIfFalse) { _, v -> v?.apply { add(worryLevel) } ?: mutableListOf(worryLevel) }
-                    }
-                }
-                items = mutableListOf()
-            }
+        fun round(worryReducer: (Long) -> Long): Map<Int, List<Long>> {
+            return worryLevels
+                .map { rules.throwItem(it, worryReducer) }
+                .onEach { inspected++ }
+                .also { worryLevels = emptyList() }
+                .groupBy({ it.first }, { it.second })
         }
 
-        fun round2(mod: Long): Map<Int, MutableList<Long>> {
-            return buildMap {
-                var listTrue = mutableListOf<Long>()
-                var listFalse = mutableListOf<Long>()
-                items.forEach {
-                    inspected++
-                    val worryLevel = operation.invoke(it) % mod
-                    if (test.test(worryLevel)) {
-                        listTrue.add(worryLevel)
-                    } else {
-                        listFalse.add(worryLevel)
-                    }
-                }
-                put(throwIfTrue, listTrue)
-                put(throwIfFalse, listFalse)
-                items.clear()
-            }
+        fun add(worryLevels: List<Long>) {
+            this.worryLevels += worryLevels
         }
-
-        companion object {
-
-            fun parse(input: String): Monkey {
-                val (rawItems, rawOperation, rawTest, rawTrueAction, rawFalseAction) = input.lines().drop(1)
-                val (divided, test) = parseTest(rawTest)
-                return Monkey(
-                    parseItems(rawItems).toMutableList(),
-                    parseOperation(rawOperation),
-                    test,
-                    parseAction(rawTrueAction),
-                    parseAction(rawFalseAction),
-                    divided
-                )
-            }
-
-            private fun parseItems(input: String): List<Long> {
-                return input.substringAfter(':').split(',').map { it.trim().toLong() }
-            }
-
-            private fun parseOperation(input: String): (worryLevel: Long) -> Long {
-                val (left, ope, right) = input.substringAfter('=')
-                    .split(' ')
-                    .map { it.trim() }
-                    .filter { it.isNotBlank() }
-                return { worryLevel ->
-                    val a = if (left == "old") worryLevel else left.toLong()
-                    val b = if (right == "old") worryLevel else right.toLong()
-                    when (ope.single()) {
-                        '+' -> a + b
-                        '*' -> a * b
-                        else -> error("Unknown operation")
-                    }
-                }
-            }
-
-            private fun parseTest(input: String): Pair<Long, Predicate<Long>> {
-                val level = input.substringAfter("divisible by").trim().toLong()
-                return level to Predicate { it % level == 0L }
-            }
-
-            private fun parseAction(input: String) = input.substringAfter("monkey ").trim().toInt()
-        }
-
     }
+
+    class Rules(
+        private val operation: (Long) -> Long,
+        private val predicate: (Long) -> Boolean,
+        private val whenTrue: Int,
+        private val whenFalse: Int
+    ) {
+
+        fun throwItem(
+            initialLevel: Long,
+            worryReducer: (Long) -> Long
+        ): Pair<Int, Long> {
+            val worryLevel = worryReducer(operation(initialLevel))
+            return if (predicate(worryLevel)) {
+                whenTrue
+            } else {
+                whenFalse
+            } to worryLevel
+        }
+    }
+
+    data class Input(val worryLevels: List<Long>, val rules: Rules, val denominator: Int)
+
+    private fun parse(input: String): Input {
+        val (rawItems, rawOperation, rawTest, rawTrueAction, rawFalseAction) = input.lines().drop(1)
+        val (denominator, test) = parseTest(rawTest)
+        return Input(
+            parseItems(rawItems),
+            Rules(
+                parseOperation(rawOperation),
+                test,
+                parseAction(rawTrueAction),
+                parseAction(rawFalseAction)
+            ),
+            denominator
+        )
+    }
+
+    private fun parseItems(input: String): List<Long> {
+        return input.substringAfter(':').split(',').map { it.trim().toLong() }
+    }
+
+    private fun parseOperation(input: String): (worryLevel: Long) -> Long {
+        val (left, ope, right) = input.substringAfter('=')
+            .split(' ')
+            .map { it.trim() }
+            .filter { it.isNotBlank() }
+        return { worryLevel ->
+            val a = if (left == "old") worryLevel else left.toLong()
+            val b = if (right == "old") worryLevel else right.toLong()
+            when (ope.single()) {
+                '+' -> a + b
+                '*' -> a * b
+                else -> error("Unknown operation")
+            }
+        }
+    }
+
+    private fun parseTest(input: String): Pair<Int, (Long) -> Boolean> {
+        val level = input.substringAfter("divisible by").trim()
+        return level.toInt() to { it % level.toLong() == 0L }
+    }
+
+    private fun parseAction(input: String) = input.substringAfter("monkey ").trim().toInt()
 }
